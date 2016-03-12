5062068dcacd1d244e74b7cbc8ef3f13dcd40ad4
stephyang <stephanie1635945@gmail.com>
2016-03-12 12:41:31 +0800

    Initial Commit

---
+++ student_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  student_test
+//
+//  Created by stephanie yang on 2016/3/12.
+//  Copyright © 2016年 stephanie yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
